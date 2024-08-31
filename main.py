030c5c22ec0b97c89634576479f7f9b87ec5f409
hamasurrehman <hamasurrehman@gmail.com>
2024-08-31 13:24:27 +0500

    added interface

--- main.py
+++ main.py
@@ -25,6 +25,19 @@ def ask(input_data:dict = {
     except Exception as e:
         log.error(f"Error Occured {e}")
         raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/interface")
+async def interface(
+    input_data:dict = {
+    "userid": "murtaza@gmail.com",
+    "share": False
+   }):
+    try:
+        admin_service = AdminMentor(userid=input_data.get('userid'))
+        local_link,public_link = admin_service.interface(share=input_data.get("share",False))
+        return {"local": local_link, "public": public_link}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))    
     
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
